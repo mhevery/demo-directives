da1ceb93103668ce64da9954a9439c0c28f5353b
Misko Hevery <misko@hevery.com>
2012-09-07 16:09:42 -0700

    step-3 greet directive (solution)

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,8 +1,26 @@
 'use strict';
 
 myApp.directive('demoGreet', function($parse) {
-  return function linkFn(scope, lElement, attrs) {
-    console.log('linkingFn(', scope, lElement, attrs, ')');
-    lElement.text('Hello World');
-  }
+  return {
+    scope: true,
+    compile: function compileFn(cElement, attrs) {
+      console.log('compileFn(', cElement, attrs, ')');
+      cElement.addClass('greet');
+      return function linkFn(scope, lElement, attrs) {
+        console.log('linkingFn(', scope, lElement, attrs, ')', cElement == lElement);
+
+        scope.leak = 'LEAKING';
+
+        scope.$watch(attrs.greet, function(name) {
+          lElement.text('Hello ' + name + '!');
+        });
+
+        lElement.bind('click', function() {
+          scope.$apply(function() {
+            $parse(attrs.greet).assign(scope, 'WORLD');
+          });
+        });
+      }
+    }
+  };
 });
